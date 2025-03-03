7c021118f7b50008b1f52913ce1dc58a80bdf00e
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-03-03 23:09:57 +0530

    Function Overloading

---
+++ contracts/Exercise4.sol
@@ -0,0 +1,31 @@
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+/* Function Overloading
+
+You can have multiple definitions for the same function name in the same scope.
+the definition of the function must differ from each other by the types and/or the number of arguements in the arguement list.
+you can overload function declarations the differ only by return type 
+*/
+
+contract FunctionOverloading{
+
+    function x(uint lightSwitch, uint wallet) public{
+
+    }
+
+    function x(uint wallet)public{
+
+    }
+}
+/*
+Exercise:
+1. Create two functions with the same name that return the sum of their arguements-
+2. one function which takes two arguements and another function which takes three arguements.
+
+3. Create two other functions which can call each sum function and return their various sums .
+4. Depoy successfully deploy contract andd test results of overloading functions.
+
+*/
