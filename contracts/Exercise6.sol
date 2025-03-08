dd96288060328fb93ee4e9032f2d89f01d636725
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-03-09 00:17:29 +0530

    oracle

--- contracts/Exercise6.sol
+++ contracts/Exercise6.sol
@@ -1,4 +1,5 @@
 /*    
 
-Exercise (Secure the Random functionality from Miner Manipulation)
+Exercise (Secure the Random functionality from Miner Manipulation):
+ 1. Create a contract Oracle with an address datatype called admin and a public integer called rand
 */
